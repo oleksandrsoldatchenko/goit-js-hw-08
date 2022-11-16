e8dcefae3418494c332196bad684715b53ff18d8
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-16 16:11:35 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryContainerItems = createGalleryContainerItems(galleryItems);
+
+function createGalleryContainerItems(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<ul class="gallery">
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    loading="lazy"
+                />
+            </a>
+        </ul>`;
+    })
+    .join("");
+};
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryContainerItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    scrollZoom: false,
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+  });
