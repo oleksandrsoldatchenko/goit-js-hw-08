d93c2a5f1a9b1d0ada6fc8d742fd7568544052f0
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-18 16:49:51 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+// Додаємо бібліотеку lodash.throttle
+import throttle from 'lodash.throttle';
 
+// Створення змінних для форми і ключа локального сховища
+const formData = {};
+const FORM_LOCAL_SROREGE_KEY = 'feedback-form-state';
+
+// Ініціалізація форми
+const formEl = document.querySelector('form');
+
+// Відстежування події input і submit з використанням throttle із затримкою 500ms
+formEl.addEventListener('input', throttle(formDataInput, 500));
+formEl.addEventListener('submit', formClearAfterSubmit);
+
+// Функція для запису даних в localStorage з formData
+function formDataInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(FORM_LOCAL_SROREGE_KEY, JSON.stringify(formData));
+};
+
+// Перевірка стану сховища, і якщо там є збережені дані, заповнюються ними поля форми. В іншому випадку поля - порожні.
+formFillCheck();
+
+function formFillCheck() {
+    const dataInForm = localStorage.getItem(FORM_LOCAL_SROREGE_KEY);
+    if (dataInForm) {
+        const parceDataInForm = JSON.parse(dataInForm);
+        for (const property in parceDataInForm) {
+            if (parceDataInForm.hasOwnProperty(property)) {
+                formEl.elements[property].value = parceDataInForm[property];
+                formData[property] = parceDataInForm[property];
+            }
+        }
+    }
+};
+
+// Функція для очищення localStorage від введених даних після натискання Submit
+function formClearAfterSubmit(event) {
+    event.preventDefault();
+    formData.email = formEl.elements.email.value;
+    formData.message = formEl.elements.message.value;
+    formEl.reset();
+    localStorage.removeItem(FORM_LOCAL_SROREGE_KEY);
+};
