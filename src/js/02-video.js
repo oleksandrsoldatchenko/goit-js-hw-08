03bc5f14219e53d9e86f3d063cb4015752b812d4
oleksandrsoldatchenko <a.soldatchenko@gmail.com>
2022-11-18 10:20:46 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+// Додаємо бібліотеку пеєра і lodash
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Ініціалізуємо плеєр
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Відстежування події timeupdate - часу відтворення
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+// Запис поточного часу відео в localStorage
+function timeUpdate(event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+// Повернення з localStorage збереженого часу відео
+const getCurrentTime = (localStorage.getItem('videoplayer-current-time'));
+
+// Встановлення часу з памяті localStorage після перезавантаження
+if (getCurrentTime !== null) {
+    player.setCurrentTime(getCurrentTime)
+};
